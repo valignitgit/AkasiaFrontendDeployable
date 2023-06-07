6807d4886e2ae4b620e0a39e615888a94d0a0ae0
vandu-s <vandana.singandipe@gmail.com>
2023-06-07 16:21:53 +0530

    setup the accesstoken in util file

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-// import { getUserCredentials } from "../utils/ApiUtil";
+import { accessToken } from "../utils/AppUtil";
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL_DEV,
   headers: {
     "Content-type": "application/json",
-    Authorization:
-      "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtb2JpbGVVc2VyQHZhbGlnbml0LmNvbSIsImlhdCI6MTY4NjA0MDIyNH0.yCl0D0q_qq6p5-V80eAJjSLhm41z_n6cDUWoBew-_cI",
+    Authorization: accessToken,
   },
 });
 
--- src/utils/AppUtil.js
+++ src/utils/AppUtil.js
@@ -6,3 +6,6 @@ export const getEmptyErrorState = () => ({
 export const getCurrencyList = (currencyArray) => {
   return currencyArray.map((currency) => currency.currency_id);
 };
+
+export const accessToken =
+  "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtb2JpbGVVc2VyQHZhbGlnbml0LmNvbSIsImlhdCI6MTY4NjA0MDIyNH0.yCl0D0q_qq6p5-V80eAJjSLhm41z_n6cDUWoBew-_cI";
